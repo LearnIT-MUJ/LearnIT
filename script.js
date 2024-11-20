d1ba7711a65e1d2f613382178193ecad7115fa35
Akshat Bisht <akshatyogeshbisht@gmail.com>
2024-11-21 01:04:09 +0530

    Website Prototype -1

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript for more dynamic effects
+document.querySelector('.card').addEventListener('mouseenter', function() {
+    this.style.transform = "scale(1.05)";
+});
+
+document.querySelector('.card').addEventListener('mouseleave', function() {
+    this.style.transform = "scale(1)";
+});
